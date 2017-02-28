912d91e884689a50e59faed56e32d005e454dfd5
andela-jkimani <jackline.kimani@andela.com>
2017-02-28 18:03:03 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/App.js';
+import { AppContainer } from 'react-hot-loader';
+
+require('index.html');
+
+const container = document.getElementById('#app-container');
+
+ReactDOM(
+  <AppContainer>
+    <App />
+  </AppContainer>, container);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(
+      <AppContainer>
+        <App />
+      </AppContainer>, container
+    );
+  });
+}
